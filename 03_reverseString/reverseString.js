d1b1f0bfd5321cbf06a5bcd43c39fc5fbc1530f9
Adem Kedir <ademkedir724@gmail.com>
2023-11-29 01:25:39 +0300

    create function that reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (words) {
+  let reversedString = "";
+  for (let i = words.length - 1; i >= 0; i--) {
+    reversedString += words[i];
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
