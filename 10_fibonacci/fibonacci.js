b3df3e21781d4d3f8f1073775f705ed08645d173
Adem Kedir <ademkedir724@gmail.com>
2023-11-29 02:34:18 +0300

    create function that tells fibonaci number of parameter

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  if (Number.isNaN(+number) || number < 0) return "OOPS";
+  number = Number(number);
 
+  if (number == 2 || number == 1) return 1;
+  else return fibonacci(number - 1) + fibonacci(number - 2);
 };
 
 // Do not edit below this line
