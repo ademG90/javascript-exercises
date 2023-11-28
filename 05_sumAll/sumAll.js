cc322e028a4e49e8de2451f91895d3286ddbdff2
Adem Kedir <ademkedir724@gmail.com>
2023-11-29 01:31:50 +0300

    add each value between parameter if they are number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (small, big) {
+  if (typeof small !== "number" || typeof big !== "number" || small * big < 0) {
+    return "ERROR";
+  }
+  if (small > big) {
+    const temp = small;
+    small = big;
+    big = temp;
+  }
+  let sumation = 0;
+  for (let i = small; i <= big; i++) {
+    sumation += i;
+  }
+  return sumation;
 };
 
 // Do not edit below this line
