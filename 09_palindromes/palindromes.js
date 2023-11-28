01d9c05de106fd0eb94b0885fa7010d38b264bb6
Adem Kedir <ademkedir724@gmail.com>
2023-11-29 02:19:42 +0300

    create a function that check isPalindromes
    the function is working for string that contain space character
    but it's not working for string those contain a punctuation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  string = string.toLowerCase().split(" ").join("");
+  string = [...string].filter((ele) => /\w/).join("");
+  for (let i = 0; i <= string.length / 2; i++) {
+    if (string[i] !== string[string.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 };
-
+palindromes("adem kedIR FH");
 // Do not edit below this line
 module.exports = palindromes;
