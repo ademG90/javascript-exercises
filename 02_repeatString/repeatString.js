a7fb6a1512e0a92ce76c628a324d5d176015b95e
Adem Kedir <ademkedir724@gmail.com>
2023-11-29 01:23:48 +0300

    create function that retype n times of string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, number) {
+  let repeatedString = "";
+  if (number < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < number; i++) {
+    repeatedString += str;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
