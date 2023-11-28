764189b5ed9ba18b089288b65fb592401b344c56
Adem Kedir <ademkedir724@gmail.com>
2023-11-29 01:27:10 +0300

    remove some sort of value from an array
    for each value of second parameter it remove from first parameter of function using rest operator

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...removedArray) {
+  return array.filter((a) => {
+    return !removedArray.includes(a);
+  });
 };
+[1, 2, 3, 4, 5, 6, 7, 8].forEach((a, b, c) => {
+  console.log(a + " " + b + " " + c);
+});
 
 // Do not edit below this line
-module.exports = removeFromArray;
+// module.exports = removeFromArray;
