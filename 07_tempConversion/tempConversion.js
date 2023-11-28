1b5d02e65b8de563aa536a3b6e5608449e501ca4
Adem Kedir <ademkedir724@gmail.com>
2023-11-29 01:36:34 +0300

    change temperature unit from one type to another
    change from degree celsius to degree fahrenheit and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  const celsius = (5 / 9) * (fahrenheit - 32);
+  if (Number.isInteger(celsius)) {
+    return celsius;
+  } else {
+    return +celsius.toFixed(1);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  const fahrenheit = 1.8 * celsius + 32;
+  if (Number.isInteger(fahrenheit)) {
+    return fahrenheit;
+  } else {
+    return +fahrenheit.toFixed(1);
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
