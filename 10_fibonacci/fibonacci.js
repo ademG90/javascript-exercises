6ef58713981cafd4fd4ed9f193a0f67cdbf06e7e
Adem Kedir <ademkedir724@gmail.com>
2023-11-29 16:19:18 +0300

    sovle function that return fibonaci series

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,7 +1,7 @@
 const fibonacci = function (number) {
   if (Number.isNaN(+number) || number < 0) return "OOPS";
   number = Number(number);
-
+  if (number === 0) return 0;
   if (number == 2 || number == 1) return 1;
   else return fibonacci(number - 1) + fibonacci(number - 2);
 };
