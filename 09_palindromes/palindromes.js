fc1ca062791bfd00473f5b361972a0032da61a65
Adem Kedir <ademkedir724@gmail.com>
2023-12-04 21:05:45 +0300

    solve get exercise and palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
 const palindromes = function (string) {
-  string = string.toLowerCase().split(" ").join("");
-  string = [...string].filter((ele) => /\w/).join("");
+  string = string.toLowerCase();
+
+  let punctRE =
+    /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
+  let spaceRE = /\s+/g;
+  string = string.toLowerCase();
+  string = string.replace(punctRE, "").replace(spaceRE, "");
   for (let i = 0; i <= string.length / 2; i++) {
     if (string[i] !== string[string.length - 1 - i]) {
       return false;
@@ -8,6 +13,5 @@ const palindromes = function (string) {
   }
   return true;
 };
-palindromes("adem kedIR FH");
-// Do not edit below this line
+
 module.exports = palindromes;
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  const book = books.map((ele) => {
+    return ele.title;
+  });
+  return book;
 };
 
 // Do not edit below this line
